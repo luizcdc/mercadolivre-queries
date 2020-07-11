c54406e60a3b7f13930169a9044782a8afb5aa6e
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-07-11 17:42:06 -0300

    Small cosmetic changes to improve readability
    The code is not 100% PEP8 compliant, but in this edit, readability was
    improved with simple changes, such as line breaks, dividing strings into
    multiple lines, etc. No algorithmic changes were made.

--- ML_scraper.py
+++ ML_scraper.py
@@ -5,16 +5,24 @@ from time import sleep
 from pickle import load
 from urllib.parse import quote
 
-SKIP_PAGES = 0  # 0 unless debugging
+SKIP_PAGES = 1500  # 0 unless debugging
 INT32_MAX = 2147483647
-KEYS_PORTUGUESE = {'link': 'Link', 'title': 'Título', 'price': 'Preço', 'no-interest': 'Parcelamento sem Juros',
-                   'in-sale': 'Em Promoção', 'reputable': 'Boa reputação', 'picture': 'Link da imagem', 'free-shipping': 'Frete Grátis'}
+KEYS_PORTUGUESE = {'link': 'Link',
+                   'title': 'Título',
+                   'price': 'Preço',
+                   'no-interest': 'Parcelamento sem Juros',
+                   'in-sale': 'Em Promoção',
+                   'reputable': 'Boa reputação',
+                   'picture': 'Link da imagem',
+                   'free-shipping': 'Frete Grátis'}
 try:
     with open("categories.pickle", "rb") as cat:
         CATS = load(cat)
 except FileNotFoundError:
-    raise FileNotFoundError(
-        "The categories.pickle database could not be loaded. Try to generate a new updated database with the extract_categories script, or to reinstall the module.")
+    raise FileNotFoundError("The categories.pickle database could not be "
+                            "loaded. Try to generate a new updated data"
+                            "base with the extract_categories script, or "
+                            "to reinstall the module.")
 
 
 def get_link(product):
@@ -36,15 +44,18 @@ def get_price(product):
 
 
 def get_picture(product):
-    picture = product.find(class_="item__image item__image--stack").find("img").get("src")
+    picture = (product.find(class_="item__image item__image--stack")
+               .find("img").get("src"))
     if not picture:
-        picture = product.find(class_="item__image item__image--stack").find("img")["data-src"]
+        picture = (product.find(class_="item__image item__image--stack")
+                   .find("img").get("data-src"))
     return picture
 
 
 def is_no_interest(product):
-    return True if product.find(class_="stack_column_item installments highlighted").contents[0].get(
-        "class") == "item-installments free-interest" else False
+    class_ = (product.find(class_="stack_column_item installments highlighted")
+              .contents[0].get("class"))
+    return class_ == "item-installments free-interest"
 
 
 def has_free_shipping(product):
@@ -57,10 +68,9 @@ def is_in_sale(product):
 
 def get_all_products(pages, min_rep):
     products = [
-        BeautifulSoup(
-            page,
-            "html.parser").find_all(
-            class_="results-item highlighted article stack product") for page in pages]
+        BeautifulSoup(page, "html.parser")
+        .find_all(class_="results-item highlighted article stack product")
+        for page in pages]
 
     return [{
             "link": get_link(product),
@@ -71,18 +81,18 @@ def get_all_products(pages, min_rep):
             "in-sale": is_in_sale(product),
             "reputable": is_reputable(
                 get_link(product), min_rep),
-            "picture": get_picture(product)} for page in products for product in page]
+            "picture": get_picture(product)}
+            for page in products for product in page]
 
 
 def is_reputable(link, min_rep=3, aggressiveness=2):
+    sleep(0.5**aggressiveness)
     product_page = BeautifulSoup(get(link).text, "html.parser")
     thermometer = str(
-        product_page.find(
-            class_="card-section seller-thermometer"))
-    sleep(0.5**aggressiveness)
+        product_page.find(class_="card-section seller-thermometer"))
     THERM_LEVELS = ("newbie", "red", "orange",
-                    "yellow", "light_green", "green")
-    if any(badrep in thermometer for badrep in (THERM_LEVELS[i] for i in range(min_rep))):
+                    "yellow", "light_green", "green")[0:min_rep]
+    if any(badrep in thermometer for badrep in THERM_LEVELS):
         return False
 
     return True
@@ -121,27 +131,33 @@ def get_cat(catid):
     return subdomain, suffix
 
 
-def get_search_pages(term, cat='0.0', price_min=0, price_max=INT32_MAX, condition=0, aggressiveness=2):
+def get_search_pages(term, cat='0.0',
+                     price_min=0, price_max=INT32_MAX,
+                     condition=0, aggressiveness=2):
     CONDITIONS = ["", "_ITEM*CONDITION_2230284", "_ITEM*CONDITION_2230581"]
     subdomain, suffix = get_cat(cat)
     index = 1
     pages = []
     while True:
+        sleep(0.5**aggressiveness)
         page = get(
-            f"https://{subdomain}.mercadolivre.com.br/{suffix}{quote(term, safe='')}_Desde_{index}_PriceRange_{price_min}-{price_max}{CONDITIONS[condition]}")
+            f"https://{subdomain}.mercadolivre.com.br/{suffix}"
+            f"{quote(term, safe='')}_Desde_{index}"
+            f"_PriceRange_{price_min}-{price_max}{CONDITIONS[condition]}")
         index += 50 * (SKIP_PAGES + 1)  # DEBUG
         if page.status_code == 404:
             break
         else:
             pages.append(page.text)
-        sleep(0.5**aggressiveness)
     return pages
 
 
 def get_parameters():
     try:
         price_min = int(input(
-            "Digite como um número inteiro, sem outros símbolos, o preço mínimo para os resultados da pesquisa (Ex: '150' sem aspas para R$ 150,00): "))
+            "Digite como um número inteiro, sem outros símbolos, o preço mínimo"
+            " para os resultados da pesquisa (Ex: '150' sem aspas para "
+            "R$ 150,00): "))
     except ValueError:
         price_min = 0
     if price_min < 0:
@@ -149,21 +165,25 @@ def get_parameters():
 
     try:
         price_max = int(input(
-            "Digite como um número inteiro, sem outros símbolos, o preço máximo para os resultados da pesquisa (Ex: '1500' sem aspas para R$ 1500,00): "))
+            "Digite como um número inteiro, sem outros símbolos, o preço máximo"
+            " para os resultados da pesquisa (Ex: '1500' sem aspas para "
+            "R$ 1500,00): "))
     except ValueError:
         price_max = INT32_MAX
     if price_max < 0:
         raise ValueError("O preço máximo deve ser um valor positivo.")
     if price_max > INT32_MAX:
         raise ValueError(
-            f"Valor muito grande: o preço máximo deve ser um valor menor que {INT32_MAX}")
+            f"Valor muito grande: o preço máximo deve ser "
+            f"um valor menor que {INT32_MAX}")
 
     if price_min > price_max:
         price_min, price_max = price_max, price_min
 
     print_cats()
     category = input(
-        "Insira a categoria de acordo com os código identificadores exibidos\n(Ex: Caso queira a categoria \"Adultos\", digite '31.1' sem aspas): ")
+        "Insira a categoria de acordo com os código identificadores exibidos\n"
+        "(Ex: Caso queira a categoria \"Adultos\", digite '31.1' sem aspas): ")
     try:
         map(int, category.split('.'))
     except ValueError:
@@ -173,7 +193,9 @@ def get_parameters():
 
     try:
         aggressiveness = int(input(
-            "Insira, entre 1 a 3 o nível desejado de agressividade:\n(Cuidado! Em um nível de agressividade alto, você pode ser bloqueado! )"))
+            "Insira, entre 1 a 3 o nível desejado de agressividade:\n"
+            "(Cuidado! Em um nível de agressividade alto, você pode ser"
+            " bloqueado! )"))
     except ValueError:
         aggressiveness = 2
     if aggressiveness not in (1, 2, 3):
@@ -182,9 +204,14 @@ def get_parameters():
     return category, price_min, price_max, condition, aggressiveness
 
 
-def ML_query(search_term, order=1, min_rep=3, category='0.0', price_min=0, price_max=INT32_MAX, condition=0, aggressiveness=2):
+def ML_query(search_term, order=1,
+             min_rep=3, category='0.0',
+             price_min=0, price_max=INT32_MAX,
+             condition=0, aggressiveness=2):
     products = get_all_products(get_search_pages(search_term, category,
-                                                 price_min, price_max, condition, aggressiveness), min_rep)
+                                                 price_min, price_max,
+                                                 condition, aggressiveness),
+                                min_rep)
     return sorted(products, key=lambda p: p["price"], reverse=(order == 2))
 
 
@@ -192,17 +219,20 @@ if __name__ == "__main__":
     while True:
         search_term = input("Digite os termos da pesquisa: ")
         advanced_mode = input(
-            "Deseja utilizar as opções avançadas de pesquisa? Digite \"sim\" se positivo: ")
+            "Deseja utilizar as opções avançadas de pesquisa? "
+            "Digite \"sim\" se positivo: ")
         if 'sim' in advanced_mode.lower():
             args = get_parameters()
             try:
                 order = int(input(
-                    "Insira a ordenação desejada dos resultados.\n(0 - relevância | 1 - preço mínimo | 2 - preço máximo): "))
+                    "Insira a ordenação desejada dos resultados.\n"
+                    "(0 - relevância | 1 - preço mínimo | 2 - preço máximo): "))
             except ValueError:
                 order = 1
             try:
                 min_rep = int(input(
-                    "Insira, entre 0 a 6, qual é o nível mínimo de reputação desejada para os vendedores: "))
+                    "Insira, entre 0 a 6, qual é o nível mínimo "
+                    "de reputação desejada para os vendedores: "))
             except ValueError:
                 min_rep = 3
         else:
@@ -217,6 +247,8 @@ if __name__ == "__main__":
                 print_product(product)
                 print()
 
-        if input("Deseja encerrar ou fazer outra pesquisa? Se quer fazer outra pesquisa, digite \"sim\" sem aspas. ").lower().strip() in "sim":
+        if input("Deseja encerrar ou fazer outra pesquisa? "
+                 "Se quer fazer outra pesquisa, "
+                 "digite \"sim\" sem aspas. ").lower().strip() in "sim":
             continue
         break
