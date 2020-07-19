22da45c6447fc9d1199637554e20491108efcdf3
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-07-19 20:01:04 -0300

    Improve user input prompts

--- search_on_ml.py
+++ search_on_ml.py
@@ -8,25 +8,18 @@ def format_price(price):
 
 
 def print_product(product):
+    price = format_price(product['price'])
     print(f"Título: {product['title']}\n" +
-          f"Preço: {format_price(product['price'])}\n" +
-          f"Em promoção: {'Sim' if product['in-sale'] else 'Não'}" +
-          " " * 28 +
-          f"Frete grátis: {'Sim' if product['free-shipping'] else 'Não'}\n" +
+          f"Preço: {price}" +
+          " " * (23 - len(price)) +  # does some math because price varies
+          f"Frete grátis: {'Sim' if product['free-shipping'] else 'Não'}" +
+          " " * 13 +
+          f"Em promoção: {'Sim' if product['in-sale'] else 'Não'}\n" +
           f"Boa reputação: {'Sim' if product['reputable'] else 'Não'}" +
-          " " * 26 +
+          " " * 12 +
           f"Sem Juros: {'Sim' if product['no-interest'] else 'Não'}\n" +
-          f"Link: {product['link'][8:]}\n" +  # doesn't print the https:// prefix
+          f"Link: {product['link'][8:]}\n" +  # doesn't print https://
           f"Imagem: {product['picture'][8:]}")  # doesn't print https://
-    # print(f"""\
-    # Título: {product['title']}
-    # Preço: {format_price(product['price'])}
-    # Em promoção: {"Sim" if product['in-sale'] else "Não"}
-    # Frete grátis: {"Sim" if product['free-shipping'] else "Não"}
-    # Boa reputação: {"Sim" if product['reputable'] else "Não"}
-    # Sem Juros: {"Sim" if product['no-interest'] else "Não"}
-    # Link: {product['link']}
-    # Imagem: {product['picture']}""")
 
 
 def print_cats():
@@ -48,7 +41,7 @@ def read_parameters():
         price_min = 0
     if price_min < 0:
         raise ValueError("O preço mínimo não pode ser um valor negativo.")
-
+    print()
     try:
         price_max = int(input(
             "Digite como um número inteiro, sem outros símbolos, o preço máximo"
@@ -62,6 +55,7 @@ def read_parameters():
         raise ValueError(
             f"Valor muito grande: o preço máximo deve ser "
             f"um valor menor que {ml_brasil.parse.INT32_MAX}")
+    print()
 
     if price_min > price_max:
         price_min, price_max = price_max, price_min
@@ -76,10 +70,11 @@ def read_parameters():
         raise ValueError("A categoria foi digitada incorretamente.")
     if len(category.split('.')) != 2:
         raise ValueError("A categoria foi digitada incorretamente.")
+    print()
 
     while True:
-        print("Qual é a condição desejada para os produtos?\n"
-              "0 - ambos\n1 - novo\n2 - usado\n")
+        print("Escolha entre 0 - ambos, 1 - novo, 2 - usado, a condição "
+              "desejada para os produtos: ")
         try:
             condition = int(input())
             if condition in (0, 1, 2):
@@ -87,16 +82,18 @@ def read_parameters():
         except ValueError:
             pass
         print("Você digitou um valor inválido.")
+    print()
 
     try:
         aggressiveness = int(input(
-            "Insira, entre 1 a 3 o nível desejado de agressividade:\n"
-            "(Cuidado! Em um nível de agressividade alto, você pode ser"
-            " bloqueado! "))
+            "Cuidado! Em um nível de agressividade alto, você pode ser "
+            "bloqueado!\n"
+            "Insira, entre 1 a 3 o nível desejado de agressividade: "))
     except ValueError:
-        aggressiveness = 2
+        aggressiveness = 3
     if aggressiveness not in (1, 2, 3):
-        aggressiveness = 2
+        aggressiveness = 3
+    print()
 
     return category, price_min, price_max, condition, aggressiveness
 
@@ -106,35 +103,41 @@ if __name__ == "__main__":
         search_term = input("Digite os termos da pesquisa: ")
         advanced_mode = input(
             "Deseja utilizar as opções avançadas de pesquisa? "
-            "Digite \"sim\" se positivo: ")
+            "Digite 'sim' se positivo, ou qualquer outra coisa para ignorar: ")
         if 'sim' in advanced_mode.lower():
             args = read_parameters()
             try:
                 order = int(input(
-                    "Insira a ordenação desejada dos resultados.\n"
-                    "(0 - relevância | 1 - preço mínimo | 2 - preço máximo): "))
+                    "Dentre as opções "
+                    "0 - relevância, 1 - preço mínimo, 2 - preço máximo, "
+                    " insira a ordenação desejada dos resultados: "))
             except ValueError:
                 order = 1
+            print()
             try:
                 min_rep = int(input(
                     "Insira, entre 0 a 6, qual é o nível mínimo "
                     "de reputação desejada para os vendedores: "))
             except ValueError:
                 min_rep = 3
+            print()
+            print()
         else:
             args = ()
             order = 1
             min_rep = 3
 
         products = ml_brasil.ML_query(search_term, order, min_rep, *args)
-
+        print("RESULTADOS:\n")
         for product in products:
             if product["reputable"]:
                 print_product(product)
                 print()
-
-        if input("Deseja encerrar ou fazer outra pesquisa? "
-                 "Se quer fazer outra pesquisa, "
-                 "digite \"sim\" sem aspas. ").lower().strip() in "sim":
+        another = input("Deseja encerrar ou fazer outra pesquisa? "
+                        "Se quer fazer outra pesquisa, "
+                        "digite \"sim\" sem aspas. Se não, digite qualquer "
+                        "outra coisa: ")
+        if another.lower().strip() in "sim":
+            print()
             continue
         break
