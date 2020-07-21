280572b52e9057fc6f1df675daba5f3ec034b1e7
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-07-20 22:36:26 -0300

    Write docstrings for package and subpackages

--- ml_brasil/__init__.py
+++ ml_brasil/__init__.py
@@ -1,3 +1,10 @@
+"""Perform extense searches in MercadoLivre Brasil
+
+This package is able to perform searches in MercadoLivre Brasil and re-
+turn lists with items which represent product listings with detailed,
+compact and relevant information that cannot be automatically collected
+through regular searches on MercadoLivre Brasil's website.
+"""
 from ml_brasil import search
 from ml_brasil import parse
 ML_query = search.ML_query
